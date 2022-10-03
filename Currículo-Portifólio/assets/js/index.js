942d60828924c10b4418d51274465c2913dd6500
Fabio Lucas <fabiolucs19@gmail.com>
2022-10-03 19:42:13 -0300

    up css

--- Currículo-Portifólio/assets/js/index.js
+++ Currículo-Portifólio/assets/js/index.js
@@ -14,3 +14,34 @@ toggleMenu.addEventListener('click', ()=>{
 closeMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('show')
 })
+
+
+//nav menu functions
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n=> n.addEventListener('click',linkAction))
+
+
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current=>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionID = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*='+ sectionID +']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*='+ sectionID +']').classList.remove('active')
+        }
+    })
+}
