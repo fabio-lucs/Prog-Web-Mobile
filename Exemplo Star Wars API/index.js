4854fd604dca94835308c73318e8b911b4e1efbb
Fabio Lucas <fabiolucs19@gmail.com>
2022-10-05 19:44:35 -0300

    up js

--- Exemplo Star Wars API/index.js
+++ Exemplo Star Wars API/index.js
@@ -4,19 +4,41 @@ const btPesquisar = document.getElementById("btPesquisar");
 
 
 const handleClickBtPesquisar = async () => {
-    const id = parseInt(inputID.value.trim());
-    if (isNaN(id) || id <= 0) {
-        alert("ID inválido! Digite um número positivo!");
-        return;
-    }
-    const response = await fetch(`https://swapi.dev/api/people/${id}`);
-    const responseLand = await fetch(`https://swapi.dev/api/people/homeworld/name${id}`);
-    const data = await response.json();
-    console.log(data);
-    h3Nome.innerHTML += "Nome = " + data.name + "<br>";
-    h3Nome.innerHTML += "Altura = " + data.height + " cm " + "<br>";
-    h3Nome.innerHTML += "Peso = " + data.mass + " kg " + "<hr><br>";
+  const id = parseInt(inputID.value.trim());
+  if (isNaN(id) || id <= 0) {
+    alert("ID inválido! Digite um número positivo!");
+    return;
+  }
+  const response = await fetch(`https://swapi.dev/api/people/${id}`);
+  const responsePlanet = await fetch(`https://swapi.dev/api/planets/${id}/`);
+  const data = await response.json();
+  const homeworld = await responsePlanet.json()
+  let films = data.films
+  let filmsListString = []
+  for (let index = 0; index < films.length; index++) {
+    
+      let film = await getJson(films[index])
+      let title = await film.title
+      filmsListString.push(title)
+  }
+
+  console.log(data);
+  console.log(homeworld)
+  console.log(filmsListString)
+  h3Nome.innerHTML += "Nome : " + data.name + "<br>";
+  h3Nome.innerHTML += "Altura : " + data.height + " cm " + "<br>";
+  h3Nome.innerHTML += "Peso : " + data.mass + " kg " + "<br>";
+  h3Nome.innerHTML += "Planeta Natal : " + homeworld.name + "<br>";
+  h3Nome.innerHTML += "Lista de filmes : " + filmsListString + "<hr><br>";
 };
+
+async function getJson(addressRequest) {
+  const response = await fetch(addressRequest);
+  const data = await response.json();
+  return data;
+}
+
+
 
 
 btPesquisar.onclick = handleClickBtPesquisar;
