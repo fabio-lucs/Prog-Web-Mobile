326c4be70a31e5e1d86e74d638c9897a382f69bf
Fabio Lucas <fabiolucs19@gmail.com>
2022-10-05 18:53:09 -0300

    up section

--- Exemplo Star Wars API/index.js
+++ Exemplo Star Wars API/index.js
@@ -1,15 +1,7 @@
 const h3Nome = document.getElementById("h3Nome");
 const inputID = document.getElementById("inputID");
 const btPesquisar = document.getElementById("btPesquisar");
-//const imgDog = document.getElementById("imgDog");
-//const pLink = document.getElementById("pLink");
 
-// const getRandomDogPicture = async () => {
-//   const response = await fetch("https://random.dog/woof.json");
-//   const data = await response.json();
-//   imgDog.src = data.url;
-//   pLink.innerHTML = data.url;
-// };
 
 const handleClickBtPesquisar = async () => {
     const id = parseInt(inputID.value.trim());
@@ -29,22 +21,6 @@ const handleClickBtPesquisar = async () => {
 
 btPesquisar.onclick = handleClickBtPesquisar;
 
-getRandomDogPicture();
 
-/*
 
-const handleClickBtPesquisar = () => {
-  const id = parseInt(inputID.value.trim());
-  if (isNaN(id) || id <= 0) {
-    alert("ID inválido! Digite um número positivo!");
-    return;
-  }
-  fetch(`https://swapi.dev/api/people/${id}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      h3Nome.innerHTML = "Nome = " + data.name;
-    });
-};
 
-*/
